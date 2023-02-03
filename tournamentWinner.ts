f2ff7119a277286a815c71e33de7d6f1dcfdde23
carollyb <carolly.barbosa@gmail.com>
2023-02-03 18:16:26 -0300

    :sparkles:

--- tournamentWinner.ts
+++ tournamentWinner.ts
@@ -1,25 +1,30 @@
+const HOME_TEAM_WON = 1;
 export function tournamentWinner(competitions: string[][], results: number[]) {
-  const team1 = competitions[0][0];
-  const team2 = competitions[1][0];
-  const team3 = competitions[2][0];
-  const points: { [key: string]: number } = {
-    team1: 0,
-    team2: 0,
-    team3: 0,
-  };
+  let currentBestTeam = "";
+  const scores = { [currentBestTeam]: 0 };
 
-  competitions.map((item, index) => {
-    if (results[index] === 0) {
-      points[item[1]] = points[item[1]] + 3;
-    } else {
-      points[item[0]] = points[item[0]] + 3;
+  for (let idx = 0; idx < competitions.length; idx++) {
+    const result = results[idx];
+    const [homeTeam, awayTeam] = competitions[idx];
+
+    const winningTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam;
+
+    updateScores(winningTeam, 3, scores);
+
+    if (scores[winningTeam] > scores[currentBestTeam]) {
+      currentBestTeam = winningTeam;
     }
-  });
-
-  if (points[team1] > points[team2] && points[team1] > points[team3]) {
-    return team1;
-  } else if (points[team2] > points[team1] && points[team2] > points[team3]) {
-    return team2;
   }
-  return team3;
+  return currentBestTeam;
+}
+
+function updateScores(
+  team: string,
+  points: number,
+  scores: { [team: string]: number }
+) {
+  if (!(team in scores)) {
+    scores[team] = 0;
+  }
+  scores[team] += points;
 }
