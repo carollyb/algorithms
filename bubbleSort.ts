04419268cf873dbd67e31b7acab21810c06edd04
carollyb <carolly.barbosa@gmail.com>
2023-02-06 22:41:40 -0300

    :beers:

---
+++ bubbleSort.ts
@@ -0,0 +1,42 @@
+// my solution
+export function bubbleSort(array: number[]) {
+  let swaps = true;
+  let noOfSwaps = 0;
+  while (swaps) {
+    let hadSwaps = noOfSwaps;
+    for (let i = 0; i < array.length - 1; i++) {
+      let next = array[i + 1];
+      let current = array[i];
+      if (current > next) {
+        array[i + 1] = current;
+        array[i] = next;
+        noOfSwaps++;
+      }
+    }
+    if (noOfSwaps === hadSwaps) {
+      swaps = false;
+    }
+  }
+  return array;
+}
+
+// slightly different version, with hints from algo expert
+export function bubbleSort2(array: number[]) {
+  let isSorted = false;
+  let noOfSwaps = 0;
+  while (!isSorted) {
+    isSorted = true;
+    let hadSwaps = noOfSwaps;
+    for (let i = 0; i < array.length - 1; i++) {
+      let next = array[i + 1];
+      let current = array[i];
+      if (current > next) {
+        array[i + 1] = current;
+        array[i] = next;
+        noOfSwaps++;
+        isSorted = false;
+      }
+    }
+  }
+  return array;
+}
