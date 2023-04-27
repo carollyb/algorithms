89a38e1e2d99e1dc8296ffa47a78aa0b5c2b9b7a
carollyb <carolly.barbosa@gmail.com>
2023-04-26 23:55:45 -0300

    :lipstick:

--- dijkstrasAlgorithm.ts
+++ dijkstrasAlgorithm.ts
@@ -9,3 +9,49 @@
   that you can only travel from a particular vertex to its destination - 
   not the other way around (unless the destination vertex itself has an 
   outbound edge to the original vertex). */
+export function dijkstrasAlgorithm(start: number, edges: number[][][]) {
+  let numberOfVertices = edges.length;
+  let minDistances = new Array(numberOfVertices).fill(Infinity);
+  minDistances[start] = 0;
+  let visited = new Set<number>();
+
+  while (visited.size !== numberOfVertices) {
+    let [vertex, currentMinDistance] = getVertexWithMinDistance(
+      minDistances,
+      visited
+    );
+    if (currentMinDistance === Infinity) {
+      break;
+    }
+
+    visited.add(vertex);
+    for (const edge of edges[vertex]) {
+      let [destination, distance] = edge;
+      //desestrutura os edges. se o vertice de destino estiver em visitado, continua o loop
+      if (visited.has(destination)) {
+        continue;
+      }
+      const newPathtoDistance = currentMinDistance + distance;
+      const currentDestinationDistance = minDistances[destination];
+      if (newPathtoDistance < currentDestinationDistance) {
+        minDistances[destination] = newPathtoDistance;
+      }
+    }
+  }
+  return minDistances.map((x) => (x === Infinity ? -1 : x));
+}
+
+function getVertexWithMinDistance(distances: number[], visited: Set<number>) {
+  let currentMinDistance = Infinity;
+  let vertex = -1;
+  for (const [vertexIdx, distance] of distances.entries()) {
+    if (visited.has(vertexIdx)) {
+      continue;
+    }
+    if (distance <= currentMinDistance) {
+      vertex = vertexIdx;
+      currentMinDistance = distance;
+    }
+  }
+  return [vertex, currentMinDistance];
+}
