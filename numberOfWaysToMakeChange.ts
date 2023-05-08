70e506b74a8c60ee4f18311bad2a8796ac35ceea
carollyb <carolly.barbosa@gmail.com>
2023-05-08 20:32:33 -0300

    :lipstick:

---
+++ numberOfWaysToMakeChange.ts
@@ -0,0 +1,23 @@
+/* 
+  Given an array of distinct positive integers representing coin denominations and a
+  single non-negative integer  n   representing a target amount of
+  money, write a function that returns the number of ways to make change for
+  that target amount using the given coin denominations.
+  Note that an unlimited amount of coins is at your disposal.
+  sample input:
+  n=6
+  denoms=[1,5]
+  sample output:
+  2 (1x1+1x5 and 6x1) */
+export function numberOfWaysToMakeChange(n: number, denoms: number[]) {
+  const ways: number[] = new Array(n + 1).fill(0);
+  ways[0] = 1;
+  for (let denom of denoms) {
+    for (let amount = 1; amount < n + 1; amount++) {
+      if (denom <= amount) {
+        ways[amount] += ways[amount - denom];
+      }
+    }
+  }
+  return ways[n];
+}
